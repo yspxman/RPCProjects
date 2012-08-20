ac8497491812f4e0194e452cf57d01155ade3507
ss <s>
2012-08-20 22:47:32 +0800

    init

---
+++ HelloRPC/Client/client.c
@@ -0,0 +1,73 @@
+/* file: helloc.c */
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "hello.h" 
+#include <windows.h>
+
+void main()
+{
+    RPC_STATUS status;
+    unsigned char * pszUuid             = NULL;
+    unsigned char * pszProtocolSequence = L"ncalrpc";
+    unsigned char * pszNetworkAddress   = NULL;
+    unsigned char * pszEndpoint         = L"hello";
+    unsigned char * pszOptions          = NULL;
+    unsigned char * pszStringBinding    = NULL;
+    unsigned char * pszString           = "hello, world";
+    unsigned long ulCode;
+ 
+    status = RpcStringBindingCompose(pszUuid,
+                                     pszProtocolSequence,
+                                     pszNetworkAddress,
+                                     pszEndpoint,
+                                     pszOptions,
+                                     &pszStringBinding);
+    if (status) exit(status);
+
+    status = RpcBindingFromStringBinding(pszStringBinding, &hello_IfHandle);
+ 
+	//status = RpcBindingFromStringBinding(pszStringBinding, &hello_v1_0_c_ifspec);
+ 
+
+    if (status) exit(status);
+ 
+    RpcTryExcept  
+    {
+        HelloProc(pszString);
+        Shutdown();
+    }
+    RpcExcept(1) 
+    {
+        ulCode = RpcExceptionCode();
+        printf("Runtime reported exception 0x%lx = %ld\n", ulCode, ulCode);
+    }
+    RpcEndExcept
+ 
+    status = RpcStringFree(&pszStringBinding); 
+ 
+    if (status) exit(status);
+ 
+    status = RpcBindingFree(&hello_IfHandle);
+ 
+	//RpcBindingFree(&hello_v1_0_c_ifspec);
+
+    if (status) exit(status);
+
+    exit(0);
+}
+
+/******************************************************/
+/*         MIDL allocate and free                     */
+/******************************************************/
+ 
+void __RPC_FAR * __RPC_USER midl_user_allocate(size_t len)
+{
+    return(malloc(len));
+}
+ 
+void __RPC_USER midl_user_free(void __RPC_FAR * ptr)
+{
+    free(ptr);
+}
+
